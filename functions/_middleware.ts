10b43ef5fae9bff564637fb93a489038e4497a5b
Om Prakash Verma <omprakashverma.in@gmail.com>
2026-04-01 15:51:44 +0530

    cors2

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,7 +1,7 @@
 export const onRequest: PagesFunction = async ({ next }) => {
   const response = await next();
   
-  const csp = "default-src 'self'; script-src 'self' https://checkout.razorpay.com https://www.gstatic.com https://apis.google.com 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://picsum.photos https://*.googleusercontent.com https://www.gstatic.com https://cdn.fynd.com; connect-src 'self' https://*.firebaseio.com https://*.googleapis.com https://api.razorpay.com https://lumberjack.razorpay.com; frame-src 'self' https://api.razorpay.com https://*.firebaseapp.com https://*.google.com; base-uri 'self'; form-action 'self';";
+  const csp = "default-src 'self'; script-src 'self' https://checkout.razorpay.com https://cdn.razorpay.com https://www.gstatic.com https://apis.google.com 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://picsum.photos https://images.unsplash.com https://*.googleusercontent.com https://www.gstatic.com https://cdn.fynd.com; connect-src 'self' https://*.firebaseio.com https://*.googleapis.com https://api.razorpay.com https://lumberjack.razorpay.com; frame-src 'self' https://api.razorpay.com https://*.firebaseapp.com https://*.google.com; base-uri 'self'; form-action 'self';";
 
   // Security Headers
   response.headers.set("Content-Security-Policy", csp);
